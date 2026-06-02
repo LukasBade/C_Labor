29e8442d23b58ee555808f6b13c29e87e8a2cb35
Lukas Bade <lukas.f.bade@gmail.com>
2026-06-02 19:20:20 +0200

    Testing our different statements regarding pointers

--- Labor07/Test.c
+++ Labor07/Test.c
@@ -1,11 +1,7 @@
 #include <stdio.h>
 int main(int argc, char* argv[])
 {
-    int i = 0, sum = 0;
-    while (i <= 3)
-    {
-        sum = sum + i;
-        ++i;
-    }
-    printf("Resultat: %d", sum);
+    int a = 0;
+    int* pointer = a;
+    printf("Resultat: %d \n", *pointer);
 }
